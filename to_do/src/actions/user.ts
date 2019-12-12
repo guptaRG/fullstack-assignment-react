e0ad15617c9e32f33ff8bcc374b36f9ee8242aae
Rishabh Gupta <rishabh.gupta@hashedin.com>
2019-12-12 13:16:15 +0530

    router history

--- to_do/src/actions/user.ts
+++ to_do/src/actions/user.ts
@@ -3,6 +3,7 @@ import { USER_CONSTANTS } from '../constants';
 import alertActions from './alert';
 import User from '../models/user';
 import { ActionDispatch } from '../types';
+import { URLS } from '../components/Routing';
 import { history } from "../_helpers/history";
 
 const login = (username:string, password:string):ActionDispatch => {
@@ -16,7 +17,7 @@ const login = (username:string, password:string):ActionDispatch => {
         userService.login(username, password).then(
             user => { 
                 dispatch(success(user));
-                history.push('/home');
+                history.push(URLS.HOME)
             },
             error => {
                 dispatch(failure(error));
--- to_do/src/components/Routing.tsx
+++ to_do/src/components/Routing.tsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { Login } from './Login';
 import { Theme } from '../types';
 import { Home } from './Home';
+import { history } from "../_helpers/history";
+
+export const URLS = {
+  LOGIN: "/",
+  HOME: "/home"
+}
 
 class Routing extends Component<{theme: Theme}> {
   render() {
     return (
-      <Router>
+      <Router history={ history }>
         <Switch>
-          <Route exact path="/" render={() => <Login theme={this.props.theme} />} />
-          <Route path="/home" render={() => <Home theme={this.props.theme} />} />
+          <Route exact path={ URLS.LOGIN } render={() => <Login theme={this.props.theme} />} />
+          <Route path={ URLS.HOME } render={() => <Home theme={this.props.theme} />} />
         </Switch>
       </Router>
     );
