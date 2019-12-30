99009dbd08e5ea2dcab806a0887598809999eb3e
Rishabh Gupta <guptarishabh.rg@gmail.com>
2019-12-30 12:36:33 +0530

    Added extra error handling in login service

--- to_do/src/services/user.ts
+++ to_do/src/services/user.ts
@@ -3,10 +3,17 @@ import {API} from '../config';
 import {API_URLS} from '../constants';
 import { AxiosResponse } from "axios";
 import User from "../models/user";
+import { detect, BrowserInfo, BotInfo, NodeInfo } from "detect-browser";
 
 const login = async (username: string, password: string): Promise<User> => {
     const requestOptions = await getRequestOptions('POST')
-    const userBody = JSON.stringify({username, password})
+    const browser:BrowserInfo | BotInfo | NodeInfo | null = detect()
+
+    const userBody = JSON.stringify({
+        username,
+        password,
+        browser_info: browser
+    })
     try {
         let loginResponse = API.post(API_URLS.USER_LOGIN, userBody, requestOptions)
         const tokenResponse = API.post(API_URLS.USER_TOKEN, userBody, requestOptions)
@@ -15,7 +22,24 @@ const login = async (username: string, password: string): Promise<User> => {
         localStorage.setItem('user', JSON.stringify(userDetails))
         return userDetails
     } catch (e) {
-        let errorMsg = e.response.data.message ? e.response.data.message : e.response.data.non_field_errors[0]
+        let errorMsg = e.toString()
+        if (e.response) {
+            if (e.response.data.message) {
+                errorMsg = e.response.data.message
+            } else if (e.response.data.non_field_errors) {
+                errorMsg = e.response.data.non_field_errors[0]
+            } else {
+                let errorArr: Array<String> = []
+                for (let key in e.response.data) {
+                    e.response.data[key].forEach((err: String) => {
+                        errorArr.push(`${key}: ${err}`)
+                    });
+                }
+                errorMsg = errorArr.join(' ')
+            }
+        }
+        console.log(errorMsg, "dasdsadas")
+
         return Promise.reject(errorMsg)
     }
 }
